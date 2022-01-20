0cb26b067f540f17e1210c4bb6332402d032d8eb
Wladiosan <w.ohrimenko@gmail.com>
2022-01-20 20:50:18 +0200

    add getAll method in movieController

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -24,6 +24,31 @@ class MovieController {
     }
 
     async getAll(req, res, next) {
+        let {title, year, limit, page} = req.body
+        let movies
+        let offset
+
+        page = page || 1
+        limit = limit || 10
+        offset = page * limit - limit
+
+        if (!title && !year) {
+            movies = await Movies.findAndCountAll({limit, offset})
+        }
+
+        if (title && !year) {
+            movies = await Movies.findAndCountAll({where: {title}, limit, offset})
+        }
+
+        if (!title && year) {
+            movies = await Movies.findAndCountAll({where: {year}, limit, offset})
+        }
+
+        if (title && year) {
+            movies = await Movies.findAndCountAll({where: {title, year}, limit, offset})
+        }
+
+        return await res.json(movies)
 
     }
 
