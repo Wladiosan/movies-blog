866a2e52f7257895c9775051394d42926d032efe
Wladiosan <w.ohrimenko@gmail.com>
2022-01-20 20:02:46 +0200

    add create function

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,7 +1,22 @@
+const {Movies} = require('../models/models')
+
 class MovieController {
 
     async create(req, res) {
 
+        try {
+
+            const {title, year, format} = req.body
+
+            const movie = await Movies.create({title, year, format})
+
+            return res.status(201).json({
+                movie
+            })
+
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     async import(req, res) {
