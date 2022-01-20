e3a2e6d5dfed36f925f999c3c008a233cdb55c72
Wladiosan <w.ohrimenko@gmail.com>
2022-01-20 22:09:31 +0200

    add checkRoleMiddleware

--- server/router/movieRouter.js
+++ server/router/movieRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express')
 
 const MoviesController = require('../controllers/movieController')
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
 const router = new Router()
 
-router.post('/create', MoviesController.create)
-router.post('/import', MoviesController.import)
+router.post('/create', checkRoleMiddleware, MoviesController.create)
+router.post('/import', checkRoleMiddleware, MoviesController.import)
 router.get('/', MoviesController.getAll)
 router.get('/:id', MoviesController.getById)
 
